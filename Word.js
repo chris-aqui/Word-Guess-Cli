b6f91e086e4a1af7a1f71b65b2886a9621ab526a
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-07-07 14:02:01 -0400

    final working

--- Word.js
+++ Word.js
@@ -5,3 +5,47 @@
 //   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
 
 //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+
+const letter = require("./Letter");
+
+let word = function(targetWord) {
+    this.targetWord = targetWord;
+    this.letters = [];
+    this.wordGuessed = false;
+    this.letterGeneration = function() {
+        for (let i = 0; i < targetWord.length; i++) {
+            let newLetter = new letter(this.targetWord[i]);
+            this.letters.push(newLetter);
+        };
+    };
+    this.letterGuess = function(guessedLetter) {
+        let guessCheckerCount = 0;
+        for (let i = 0; i < this.letters.length; i++) {
+            if (this.letters[i].character === guessedLetter) {
+                this.letters[i].guessed = true;
+                guessCheckerCount++;
+            };
+        };
+        return guessCheckerCount;
+    };
+    this.winCheck = function() {
+        this.wordGuessed = this.letters.every(function(currentLetter) {
+            return currentLetter.guessed;
+        });
+        return this.wordGuessed;
+    };
+    this.renderWord = function() {
+        let renderedWord = [];
+
+        for (let i in this.letters) {
+            if (this.letters[i].guessed){
+                renderedWord += this.letters[i].character;
+            } else {
+                renderedWord += "_ ";
+            };
+        };
+        return renderedWord;
+    };
+};
+//
+module.exports = word;
--- index.js
+++ index.js
@@ -3,3 +3,49 @@
 //   * Randomly selects a word and uses the `Word` constructor to store it
 
 //   * Prompts the user for each guess and keeps track of the user's remaining guesses
+const word = require("./Word");
+const inquirer = require("Inquirer");
+//
+let game = {
+  wordBank: ["charmander", "bulbasaur ", "squirtle", "pikachu"],
+  guessesRemaining: 10,
+  currentWord: null,
+  startGame: function (wordz) {
+    this.guessesRemaining = 10;
+    this.currentWord = new word(this.wordBank[Math.floor(Math.random() * this.wordBank.length)]);
+    this.currentWord.letterGeneration();
+    this.userGuess();
+  },
+  userGuess: function () {
+    let thisThing = this;
+    inquirer.prompt([{
+      name: 'guessedLetter',
+      type: 'input',
+      message: 'What letter would you like to guess?'
+    }]).then(function (response) {
+      // console.log("the current wordfor testing ", );
+      console.log(response.guessedLetter);
+      let guessCheckerProcess = thisThing.currentWord.letterGuess(response.guessedLetter);
+      if (guessCheckerProcess === 0) {
+        thisThing.guessesRemaining--;
+        console.log("Guesses Remaining: " + thisThing.guessesRemaining);
+      } else {
+        console.log("Good Guess!");
+        if (thisThing.currentWord.winCheck()) {
+          console.log("You win!");
+        };
+      };
+      if (thisThing.guessesRemaining > 0 && thisThing.currentWord.wordGuessed === false) {
+        thisThing.userGuess();
+      };
+      if (thisThing.guessesRemaining === 0) {
+        console.log("Game over. The word was: " + thisThing.currentWord.targetWord);
+        return;
+      } else {
+        console.log(thisThing.currentWord.renderWord());
+      };
+    });
+  }
+};
+//
+game.startGame();
